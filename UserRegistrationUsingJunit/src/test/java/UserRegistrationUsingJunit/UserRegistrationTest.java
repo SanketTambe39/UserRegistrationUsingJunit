033d95eefe07164fb8ee1f079573efc3e5d8af95
SanketTambe39 <sankettambe39@gmail.com>
2021-07-13 17:10:56 +0530

    ADDED: Test case for email validation

--- UserRegistrationUsingJunit/src/test/java/UserRegistrationUsingJunit/UserRegistrationTest.java
+++ UserRegistrationUsingJunit/src/test/java/UserRegistrationUsingJunit/UserRegistrationTest.java
@@ -6,74 +6,66 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
 	UserValidationImpl validator = new UserValidationImpl();
-	
-	//checking condition for first name
+	// checking condition for first name
 	@Test
-	public void givenFirstName_WhenProper_ShouldReturnTrue() 
-	{
-		Assert.assertTrue(validator.checkFirstName("Abcd"));			
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		Assert.assertTrue(validator.checkFirstName("Abcd"));
 	}
 
 	@Test
-	public void givenFirstName_WhenShort_ShouldReturnFalse() 
-	{
-		Assert.assertFalse(validator.checkFirstName("Ab"));			
+	public void givenFirstName_WhenShort_ShouldReturnFalse() {
+		Assert.assertFalse(validator.checkFirstName("Ab"));
 	}
 
 	@Test
-	public void givenFirstName_WhenFirstNotInUpperCase_ShouldReturnFalse() 
-	{
-		Assert.assertFalse(validator.checkFirstName("abcd"));			
+	public void givenFirstName_WhenFirstNotInUpperCase_ShouldReturnFalse() {
+		Assert.assertFalse(validator.checkFirstName("abcd"));
 	}
-	//checking condition for first name
+
+	// checking condition for first name
 	@Test
-	public void givenLastName_WhenProper_ShouldReturnTrue() 
-	{
-		Assert.assertTrue(validator.checkLastName("Abcd"));			
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		Assert.assertTrue(validator.checkLastName("Abcd"));
 	}
+
 	@Test
-	public void givenLastName_WhenShort_ShouldReturnFalse() 
-	{
-		Assert.assertFalse(validator.checkLastName("Ab"));			
+	public void givenLastName_WhenShort_ShouldReturnFalse() {
+		Assert.assertFalse(validator.checkLastName("Ab"));
 	}
+
 	@Test
-	public void givenLastName_WhenFirstNotInUpperCase_ShouldReturnFalse() 
-	{
-		Assert.assertFalse(validator.checkLastName("abcd"));			
-	}	
-	//checking conditions for email
+	public void givenLastName_WhenFirstNotInUpperCase_ShouldReturnFalse() {
+		Assert.assertFalse(validator.checkLastName("abcd"));
+	}
+
+	// checking conditions for email
 	@Test
-	public void givenEmail_WhenProper_ShouldReturnTrue()
-	{
+	public void givenEmail_WhenProper_ShouldReturnTrue() {
 		Assert.assertTrue(validator.checkEmailAddress("abc.xyz@bridgelabz.co.in"));
 	}
+
 	@Test
-	public void givenEmail_WithoutUserName_ShouldReturnFalse()
-	{
+	public void givenEmail_WithoutUserName_ShouldReturnFalse() {
 		Assert.assertFalse(validator.checkEmailAddress("@bridgelabz.co.in"));
 	}
 
 	@Test
-	public void givenEmail_WithoutDomainName_ShouldReturnFalse()
-	{
+	public void givenEmail_WithoutDomainName_ShouldReturnFalse() {
 		Assert.assertFalse(validator.checkEmailAddress("abc.xyzco.in"));
 	}
 
 	@Test
-	public void givenEmail_WithoutDomain_ShouldReturnFalse()
-	{
+	public void givenEmail_WithoutDomain_ShouldReturnFalse() {
 		Assert.assertFalse(validator.checkEmailAddress("abc.xyz@bridgelabz"));
 	}
 
 	@Test
-	public void givenEmail_WithoutOptinalUserName_ShouldReturnTrue()
-	{
+	public void givenEmail_WithoutOptinalUserName_ShouldReturnTrue() {
 		Assert.assertTrue(validator.checkEmailAddress("abc@bridgelabz.co.in"));
 	}
 
 	@Test
-	public void givenEmail_WithoutOptinalDomain_ShouldReturnTrue()
-	{
+	public void givenEmail_WithoutOptinalDomain_ShouldReturnTrue() {
 		Assert.assertTrue(validator.checkEmailAddress("abc.xyz@bridgelabz.com"));
 	}
 }
