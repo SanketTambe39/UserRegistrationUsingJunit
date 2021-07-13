57209eb1d09b4cc7e274f5f7edc3c595c54f43fa
SanketTambe39 <sankettambe39@gmail.com>
2021-07-13 16:51:20 +0530

    ADDED: Test case to check last name

--- UserRegistrationUsingJunit/src/test/java/UserRegistrationUsingJunit/UserRegistrationTest.java
+++ UserRegistrationUsingJunit/src/test/java/UserRegistrationUsingJunit/UserRegistrationTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
 	UserValidationImpl namevalidator = new UserValidationImpl();
-
+	
+	//checking condition for first name
 	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() 
 	{
@@ -24,5 +25,21 @@ public class UserRegistrationTest {
 	{
 		Assert.assertFalse(namevalidator.checkFirstName("abcd"));			
 	}
+	//checking condition for first name
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() 
+	{
+		Assert.assertTrue(namevalidator.checkFirstName("Abcd"));			
+	}
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() 
+	{
+		Assert.assertFalse(namevalidator.checkFirstName("Ab"));			
+	}
+	@Test
+	public void givenLastName_WhenFirstNotInUpperCase_ShouldReturnFalse() 
+	{
+		Assert.assertFalse(namevalidator.checkFirstName("abcd"));			
+	}	
 	
 }
